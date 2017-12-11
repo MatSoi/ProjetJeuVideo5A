e173b11ed062ed8596d9c0a09d9b1fdf0dd9a1e1
MatSoi <33734431+MatSoi@users.noreply.github.com>
2017-12-11 21:55:09 +0100

    Doxygen documentation scene.h and scenedebug.h

--- project/src/scene.h
+++ project/src/scene.h
@@ -1,13 +1,19 @@
 #ifndef SCENE_H
 #define SCENE_H
-
 #include "player.h"
 #include "enemy.h"
 #include "events.h"
 #include "menu.h"
 
+/*!
+ * \file scene.h
+ * \brief Classe Scene qui comporte les differentes initialisations de la scene ainsi que la boucle principale du programme
+ * \author SOIGNON Matthieu et PASTOR Mickael
+ */
+
 #define ATTACK_DIST 50
 
+/*! Enumeration definissant des ID donnant des propriétés de selectionnabilité aux nodes */
 enum
 {
     // I use this ISceneNode ID to indicate a scene node that is
@@ -16,64 +22,107 @@ enum
 
     // I use this flag in ISceneNode IDs to indicate that the
     // scene node can be picked by ray selection.
-    IDFlag_IsPickable = 1 << 0,
-
-    // I use this flag in ISceneNode IDs to indicate that the
-    // scene node can be highlighted.  In this example, the
-    // homonids can be highlighted, but the level mesh can't.
-    IDFlag_IsHighlightable = 1 << 1
+    IDFlag_IsPickable = 1 << 0
 };
 
+/**
+ * @brief Classe Scene
+ * Represente la scene avec ses attributs et les fonctions d'initialisations ainsi que la boucle principale
+ */
 class Scene
 {
 public:
+    /**
+     * @brief Constructeur de la scene, assure la creation des composantes principales de la scene
+     */
     Scene();
+
+    /**
+     * @brief Fonction d'initialisation globale qui appelle les differentes fonctions d'initialisations elementaires
+     */
     void init();
+
+    /**
+     * @brief Boucle principale du programme
+     */
     void run();
 
 protected:
 
+    /**
+     * @brief Fonction d'initialisation de la map
+     */
     void initMap();
+
+    /**
+     * @brief Fonction d'initialisation des textures
+     */
     void initTextures();
+
+    /**
+     * @brief Fonction d'initialisation du joueur
+     */
     void initPlayer();
+
+    /**
+     * @brief Fonction d'initialisation des ennemis
+     */
     void initEnemy();
+
+    /**
+     * @brief Fonction d'initialisation de la camera
+     */
     void initCamera();
+
+    /**
+     * @brief Fonction de connection du gestionnaire d'evenements avec les pointeurs des variables necessaires
+     */
     void initReceiver();
+
+    /**
+     * @brief Fonction appellee lorsque le joueur attaque : tape l'ennemi en face si a distance < ATTACK_DIST
+     */
     void playerAttack();
+
+    /**
+     * @brief Fonction d'affichage d'element de debug dans la window de debug
+     * @param wstr : texte a afficher
+     * @param ind : indice de l'element de la window de debug a modifier
+     */
     void debugDisplay(std::wstring wstr, int ind);
 
 
     // Le gestionnaire d'événements
-    EventReceiver receiver;
-    std::vector<iv::ITexture*> textures;
+    EventReceiver receiver;/*!< event receiver */
+    std::vector<iv::ITexture*> textures;/*!< tableau des textures des personnages */
 
     // Création de la fenêtre et du système de rendu.
-    IrrlichtDevice *device;
-    iv::IVideoDriver    *driver;
-    is::ISceneManager   *smgr;
-    ig::IGUIEnvironment *gui;
-    is::ISceneCollisionManager* collMan;
-    Menu* menu;
+    IrrlichtDevice *device;/*!< pointeur sur le device */
+    iv::IVideoDriver    *driver;/*!< pointeur sur le driver */
+    is::ISceneManager   *smgr;/*!< pointeur sur le manager de scene */
+    ig::IGUIEnvironment *gui;/*!< pointeur sur l'interface graphique */
+    is::ISceneCollisionManager* collMan;/*!< pointeur sur le gestionnaire de collision */
+    Menu* menu;/*!< pointeur sur le menu */
 
-    is:: IAnimatedMesh *meshMap;
-    is:: IMeshSceneNode *nodeMap;
+    is:: IAnimatedMesh *meshMap;/*!< pointeur sur le mesh de la map */
+    is:: IMeshSceneNode *nodeMap;/*!< pointeur sur le node de la map */
 
-    is::IAnimatedMesh *meshPlayer;
-    is::IAnimatedMeshSceneNode *nodePlayer;
+    is::IAnimatedMesh *meshPlayer;/*!< pointeur sur le mesh du joueur */
+    is::IAnimatedMeshSceneNode *nodePlayer;/*!< pointeur sur le node du joueur */
 
-    is::IAnimatedMesh *meshEnemy;
-    is::IAnimatedMeshSceneNode *nodeEnemy;
+    is::IAnimatedMesh *meshEnemy;/*!< pointeur sur le mesh de l'ennemi */
+    is::IAnimatedMeshSceneNode *nodeEnemy;/*!< pointeur sur le node de l'ennemi */
 
-    is::ICameraSceneNode *camera;
+    is::ICameraSceneNode *camera;/*!< pointeur sur la camera */
 
     // Choix de la police de caractères
-    ig::IGUISkin* skin;
-    ig::IGUIFont* font;
+    ig::IGUISkin* skin;/*!< pointeur sur le style de la police d'ecriture */
+    ig::IGUIFont* font;/*!< pointeur sur la taille de la police d'ecriture */
 
-    ig::ICursorControl* cursor;
+    ig::ICursorControl* cursor;/*!< pointeur sur le curseur de la souris */
 
-    Player player;
-    Enemy enemy;
+    Player player;/*!< classe joueur */
+    Enemy enemy;/*!< classe enemy */
 
 };
 
--- project/src/scenedebug.h
+++ project/src/scenedebug.h
@@ -1,31 +1,67 @@
 #ifndef SCENEDEBUG_H
 #define SCENEDEBUG_H
 
+/*!
+ * \file scenedebug.h
+ * \brief Classe SceneDebug qui herite de la classe Scene
+ * Elle est utilise seulement pour le debug, permettant d'avoir des options de debug constamment affiche
+ * \author SOIGNON Matthieu et PASTOR Mickael
+ */
+
+
 #include "scene.h"
 #include <cmath>
 
+/**
+ * @brief Classe SceneDebug
+ * Permet l'affichage constant de certaines options de debug supplementaires
+ */
 class SceneDebug : public Scene
 {
 public:
+    /**
+     * @brief Constructeur de SceneDebug qui appelle le constructeur de Scene
+     */
     SceneDebug() : Scene() {}
+
+    /**
+     * @brief Surchage de la fonction d'initialisation de scene pour initialiser aussi les composants de debug
+     */
     void init();
+
+    /**
+     * @brief Surchage de la boucle principale pour afficher option de debug
+     */
     void run();
 
 private:
 
+    /**
+     * @brief Fonction d'initialisation des billboard utilise pour afficher la vision des ennemis
+     */
     void initBillboardDebug();
+
+    /**
+     * @brief Fonction d'initialisation des selecteurs sur les personnages
+     */
     void initSelector();
+
+    /**
+     * @brief Fonction d'initialisation des fleches d'affichages des axes x,y et z
+     */
     void initArrowDebug();
+
+    /**
+     * @brief Fonction de lancer de rayons a partir de l'ennemi afin d'afficher sa vision
+     */
     void enemyRaycastDebug();
 
-    is::ISceneNode * arrowParentDebug;
-    std::vector<is::ISceneNode *> arrowsDebug;
-    is::ISceneCollisionManager* collManDebug;
+    is::ISceneNode * arrowParentDebug;/*!< Pointeur sur le repere d'affichage des axes x,y et z */
+    std::vector<is::ISceneNode *> arrowsDebug;/*!< Tableau des pointeurs sur les axes x,y et z */
+    is::ISceneCollisionManager* collManDebug;/*!< Pointeur sur le manager de collision */
 
-    std::vector<scene::IBillboardSceneNode *> bill;
-    scene::ISceneNode* highlightedSceneNode;
-    video::SMaterial material;
-    int angleConeEnemy=20;
+    std::vector<scene::IBillboardSceneNode *> bill;/*!< Tableau de pointeurs sur les billboards d'affichage de la vision ennemi */
+    int angleConeEnemy=20;/*!< Angle de vision de l'ennemi par rapport au centre de sa vision */
 };
 
 #endif // SCENEDEBUG_H
